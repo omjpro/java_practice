b3426b84ab15cf48da5467d277918717ac024166
omjpro <omjcrow@naver.com>
2021-07-21 02:42:05 +0900

    테스트

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
+		// 커밋용 수정
 		System.out.println("hello,world.");
 	}
 
