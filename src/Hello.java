a541dcc1f0eae3f536a8e06927cb85f29c10fd86
omjpro <omjcrow@naver.com>
2021-07-23 00:14:23 +0900

    깃 테스트

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
-		// 커밋용 멘트
+		// 而ㅻ컠�슜 硫섑듃
 		System.out.println("hello,world.");
+		System.out.println("hello mogi i will find you and i will kill you");
 	}
 
 }
